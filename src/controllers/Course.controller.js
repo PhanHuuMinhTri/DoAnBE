e31c31baba61293080655042ac55efb4c38a0a53
TriPhm <phantri1617@gmail.com>
2023-06-21 13:52:14 +0700

    Add API Course

--- src/controllers/Course.controller.js
+++ src/controllers/Course.controller.js
@@ -25,6 +25,45 @@ class CourseController {
       });
     } catch (error) {}
   }
+
+  addCourse(req, res) {
+    const data = req.body;
+    try {
+      course.addCourse(data, (err, rows) => {
+        if (!err) {
+          res.status(200).send("Add course success");
+        } else {
+          res.status(500).json({ error: "error when get course from db" });
+        }
+      });
+    } catch (error) {}
+  }
+
+  updateCourse(req, res) {
+    const data = req.body;
+    try {
+      course.updateCourse(data, (err, rows) => {
+        if (!err) {
+          res.status(200).send("Update course success");
+        } else {
+          res.status(500).json({ error: "error when get course from db" });
+        }
+      });
+    } catch (error) {}
+  }
+
+  deleteCourse(req, res) {
+    const id = req.params.id;
+    try {
+      course.deleteCourse(id, (err, rows) => {
+        if (!err) {
+          res.status(200).send("Delete course success");
+        } else {
+          res.status(500).json({ error: "error when get course from db" });
+        }
+      });
+    } catch (error) {}
+  }
 }
 
 module.exports = new CourseController();
--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const CourseController = require("../controllers/Course.controller");
 
 router.get("/:id", CourseController.getCourse);
+router.delete("/delete/:id", CourseController.deleteCourse);
 router.get("/", CourseController.getAllCourse);
-
+router.post("/add", CourseController.addCourse);
+router.post("/edit", CourseController.updateCourse);
 module.exports = router;
